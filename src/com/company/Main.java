2e1ed032775444e80e7fab3bc01a33cd78d6450b
Afaq Anwar <anwarafaq@outlook.com>
2018-09-20 11:15:23 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static String hyphenMe(String str) {
+        if (str.length()%2 == 0)
+        {
+            String evenH = str.substring(0, str.length()/2);
+            String evenH2 = str.substring(str.length()/2, str.length());
+            String palinH = "";
+            for (int h = evenH.length(); h >= 0; h--) {
+                
+            }
+        }
+        else
+        {}
+    }
+}
